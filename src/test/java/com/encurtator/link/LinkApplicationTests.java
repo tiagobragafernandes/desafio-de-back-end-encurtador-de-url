3e74949a6dda3fc64fca459b6a4e72d653222c29
tiagobragafernandes <tibrafe@gmail.com>
2024-06-05 15:19:44 -0300

    initial commit

---
+++ src/test/java/com/encurtator/link/LinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encurtator.link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
