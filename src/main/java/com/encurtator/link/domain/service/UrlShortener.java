b03f02b415ac9fb197bfe7b341cba1bfa81b44c6
tiagobragafernandes <tibrafe@gmail.com>
2024-06-07 13:57:39 -0300

    refactor: changing the method to encrypt the link.

--- src/main/java/com/encurtator/link/domain/service/UrlShortener.java
+++ src/main/java/com/encurtator/link/domain/service/UrlShortener.java
@@ -6,16 +6,18 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 @Component
 public class UrlShortener {
+
+    private static final int SHORT_URL_LENGTH = 10;
+    private static final Random RANDOM = new Random();
+
     public static String generateShortUrl(String originalUrl) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(originalUrl.getBytes(StandardCharsets.UTF_8));
-            return Base64.getUrlEncoder().withoutPadding().encodeToString(hash).substring(0, 8);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Erro ao gerar URL curta", e);
-        }
+        return RANDOM.ints(SHORT_URL_LENGTH, 0, 10)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
